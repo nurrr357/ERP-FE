86b0e5112ecc2019e084d839baff944563941669
nurr357 <muhamadhuda49@gmail.com>
2025-12-04 09:52:48 +0700

    setup tailwind

---
+++ resources/views/Coba.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel App</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+
+    <div class="bg-white p-8 rounded-xl shadow-lg w-[400px] text-center">
+        <h1 class="text-3xl font-bold text-blue-600 mb-4">
+            YOA TEMPEEEEEEEEEEK
+        </h1>
+    </div>
+</body>
+
+</html>
